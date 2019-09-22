a42f10ed9cb8c6c2bbeb29622f557015dd0ad8a2
virtual-frame-7789 <mttwright@gmail.com>
2019-09-22 17:55:22 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,7 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    if person == "Free"
+    if person == "Freddy Mercury"
+      con
   end
 end
